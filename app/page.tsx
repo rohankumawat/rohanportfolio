cbba00ec658f8a38909cddcf651aa91ee67b39d7
Rohan Kumawat <kumawatrohan@gmail.com>
2024-08-31 12:57:36 +0100

    two-column first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,35 @@
 import { BlogPosts } from 'app/components/posts'
+import Image from 'next/image';
+import first from 'public/images/home/first.jpeg'
 
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        hi, I'm Rohan 👋✨
-      </h1>
-      <p className="mb-4">
-        {`thinkinggggg....!`}
-      </p>
-      <div className="my-8">
+    <section className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+        {/* Left Column */}
+        <div>
+          <h1 className="mb-4 text-4xl font-bold tracking-tight">Hi, I'm Rohan 👋</h1>
+          <p className="mb-4 text-xl font-light">
+            I’m a software developer, AI Engineer, Machine Learning Engineer, and Data Analyst.
+          </p>
+          <p className="mb-8 text-md">
+            I love creating innovative software solutions and building AI models that solve real-world problems. Join me on this journey as I explore new technologies, develop exciting projects, and share my experiences along the way. 🚀
+          </p>
+        </div>
+          {/* Right Column (Image) */}
+        <div className="flex justify-center md:justify-end">
+          <Image
+            src={first} // Replace with your actual image path
+            alt="Rohan - Uni of Glasgow"
+            className="rounded-lg shadow-lg"
+            width={300}
+            height={300}
+          />
+        </div>
+      </div>
+
+      {/* Blog Posts Section */}
+      <div className="my-16">
         <BlogPosts />
       </div>
     </section>
